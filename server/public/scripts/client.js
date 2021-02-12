8675241d2220c93b25f9e62f95810b797a2468c0
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-11 21:00:47 -0600

    Client properly catching input data and clearing textbox

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,6 +10,17 @@ function onReady() {
 }
 
 function handleSubmit(event) {
+  // prevent form from default refresh:
   event.preventDefault();
   console.log('in handleSubmit');
+
+  let newToDo = {
+    todo: $('#input-task').val(),
+    completed: false,
+  };
+
+  console.log('newToDo:', newToDo);
+
+  // Clear input box after collecting data:
+  $('#input-task').val('');
 }
