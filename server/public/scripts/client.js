abce68c174949e3ce078ec1da15b64aa56226204
McKynlee Westman <mckynlee.westman@gmail.com>
2021-02-11 20:46:26 -0600

    Properly listening to form submit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,12 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('JQ running!');
+
+  // Set up event listeners:
+  $(document).on('submit', '#task-form', handleSubmit);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('in handleSubmit');
 }
